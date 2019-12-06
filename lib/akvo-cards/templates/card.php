84b29fc1cb56a70f06cffe61058932dfe0e696f3
unknown <samvthom16@gmail.com>
2019-12-06 21:38:20 +0530

    #1 - Card layout changes

--- lib/akvo-cards/templates/card.php
+++ lib/akvo-cards/templates/card.php
@@ -7,16 +7,18 @@
 	}
 ?>
 <div class='card <?php _e(self::slugify($atts['type']));?>'>
-	<div class='card-image' <?php if($atts['img']):?>style="background-image:url('<?php _e($atts['img']);?>');"<?php endif;?>>
-		<div class="card-tag"><?php if($atts['type-text']){_e($atts['type-text']);} else{_e($atts['type']);}?></div>
-	</div>
-	<div class='card-content'>
-		<h3 class='card-title'>
-			<a href="<?php _e($atts['link']);?>"><?php _e($atts['title']);?></a>
-		</h3>
-		<?php if( strlen( $atts['title'] ) < 30 ) :?>
-		<div class="card-excerpt text-muted"><?php echo truncate($atts['content'], 130);?></div>
-		<?php endif;?>
-		<div class="card-date"><?php _e( 'Published on '. $atts['date'] );?></div>
-	</div>
+	<a href="<?php _e($atts['link']);?>">
+		<div class='card-image' <?php if($atts['img']):?>style="background-image:url('<?php _e($atts['img']);?>');"<?php endif;?>>
+			<div class="card-tag"><?php if($atts['type-text']){_e($atts['type-text']);} else{_e($atts['type']);}?></div>
+		</div>
+		<div class='card-content'>
+			<h3 class='card-title'>
+				<?php _e($atts['title']);?>
+			</h3>
+			<?php if( strlen( $atts['title'] ) < 40 ) :?>
+			<div class="card-excerpt text-muted"><?php echo truncate($atts['content'], 130);?></div>
+			<?php endif;?>
+			<div class="card-date"><?php _e( 'Published on '. $atts['date'] );?></div>
+		</div>
+	</a>
 </div>
