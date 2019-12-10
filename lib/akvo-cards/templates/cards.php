c73e1b547b764fbd8f535346c38ae1d9e31a4f78
unknown <samvthom16@gmail.com>
2019-12-10 20:42:22 +0530

    #4 - update to flex

--- lib/akvo-cards/templates/cards.php
+++ lib/akvo-cards/templates/cards.php
@@ -1,26 +1,26 @@
-<div id="cards-list" data-target="<?php if( $atts['template'] == 'list' ){_e('.col-md-12');}else{_e('.col-md-4');}?>.eq" class="row" data-url="<?php _e($url);?>" data-paged="akvo-paged">
+<div id="cards-list" data-target="<?php if( $atts['template'] == 'list' ){_e('.col-md-12');}else{_e('.col');}?>.eq" class="row-col3" data-url="<?php _e($url);?>" data-paged="akvo-paged">
 	<?php foreach($data as $item):?>
-	<div class="<?php if( $atts['template'] == 'list' ){_e('col-md-12');}else{_e('col-md-4');}?> eq">
+	<div class="<?php if( $atts['template'] == 'list' ){_e('col-md-12');}else{_e('col');}?> eq">
 	<?php
-			
+
 		/* FORM SHORTCODE */
-			
+
 		if( $atts['template'] == 'list' ){ $shortcode = '[akvo-list'; }		// choose shortcode based on the template that has been selected
 		else{ $shortcode = '[akvo-card'; }
-			
-		foreach($item as $key=>$val){	/* ADD ATTRIBUTES TO THE SHORTCODE */	
-			$shortcode .= ' '.$key.'="'.$val.'"';	
+
+		foreach($item as $key=>$val){	/* ADD ATTRIBUTES TO THE SHORTCODE */
+			$shortcode .= ' '.$key.'="'.$val.'"';
 		}
-			
-		$shortcode .= ']';					// end shortcode 
-			
+
+		$shortcode .= ']';					// end shortcode
+
 		/* FORM SHORTCODE */
-			
+
 		echo do_shortcode($shortcode);		// print shortcode
 	?>
 	</div>
 	<?php endforeach;?>
-</div>	
+</div>
 <?php if( $atts['pagination'] && count( $data ) ): /* lazy loading pagination to be enabled only when it is set in the shortcode */ ?>
 <div class="row">
 	<div class="col-sm-12 text-center">
