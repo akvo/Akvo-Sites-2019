08d8d16387efa53752f4f59e7864127ac10008e5
unknown <samvthom16@gmail.com>
2020-05-13 15:05:13 +0530

    #17 - remove square brackets

--- lib/akvo-cards/templates/cards.php
+++ lib/akvo-cards/templates/cards.php
@@ -32,6 +32,8 @@
 			else{ $shortcode = '[akvo-card'; }
 
 			foreach($item as $key=>$val){	/* ADD ATTRIBUTES TO THE SHORTCODE */
+				$val = str_replace( "[", "", $val );
+				$val = str_replace( "]", "", $val );
 				$shortcode .= ' '.$key.'="'.$val.'"';
 			}
 
