e55b721c2a99970584bc6a4230adf75646c3bd91
Steve <anil.stephen66@gmail.com>
2025-04-11 13:25:36 +0530

    #21 - Conditionally show featured image column

--- partials/single-default.php
+++ partials/single-default.php
@@ -4,7 +4,7 @@
 		<?php if(have_posts()): while ( have_posts() ) : the_post();?>
 			<?php $type = get_post_type();?>
 			<div class="row">
-				<div class="col-sm-5"><?php the_post_thumbnail();?></div>
+				<?php if( has_post_thumbnail() ): ?><div class="col-sm-5"><?php the_post_thumbnail();?></div><?php endif; ?>
 				<div class="col-sm-7">
 					<h1><?php the_title();?></h1>
 					<br>
